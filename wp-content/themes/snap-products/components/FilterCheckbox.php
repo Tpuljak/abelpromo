8295c54ced9d805b3625504d05d7b2b69587faf8
Toma Puljak <toma.puljak@hotmail.com>
2019-07-10 17:59:36 +0200

    Menu implementation

--- wp-content/themes/snap-products/components/FilterCheckbox.php
+++ wp-content/themes/snap-products/components/FilterCheckbox.php
@@ -1,5 +1,5 @@
-<?php function FilterCheckbox($string, $type) { ?>
-    <div class="filter-checkbox" type="<?php echo $type; ?>">
+<?php function FilterCheckbox($string, $type, $is_checked = false) { ?>
+    <div class="filter-checkbox <?php echo ($is_checked) ? 'checked' : ''; ?>" type="<?php echo $type; ?>">
         <input type="checkbox" name="filter" >
         <div class="checkbox"></div>
         <?php echo $string; ?>
