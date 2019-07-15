dcc63d3c2cf8d92153f05890e7aa47d3ea0e6b57
Toma Puljak <toma.puljak@hotmail.com>
2019-07-15 16:53:43 +0200

    Contact form work

--- wp-content/themes/snap-products/page-order.php
+++ wp-content/themes/snap-products/page-order.php
@@ -2,12 +2,113 @@
   global $language;
 
   update_language();
+
+  $request_params = get_request_params();
+
+  if (isset($request_params['product_id'])) {
+    $product_id = $request_params['product_id'];
+
+    $product = get_product($product_id);
+
+    if ($product) {
+        $product = array_values($product)[0];
+    }
+
+    $options = map_additional_options($product);
+
+    // var_dump($product);
+  }
 ?>
 
 <?php get_header(); ?>
 <div class="order-page-wrapper">
     <?php Sidebar('vertical'); ?>
-    <?php echo do_shortcode('[contact-form-7 id="165" title="Order"]');?>
+    <main class="order-form">
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'Proizvod' : 'Product'; ?></label>
+            <input type="text" placeholder="<?php echo ($language == 'HR') ? 'Ime proizvoda' : 'Product name';?>" value="<?php echo $product->title; ?>">
+        </div>
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'Boja p.' : 'P. color'; ?></label>
+            <select>
+                <?php
+                    foreach ($product->images as $img) {
+                        $colour = $img->colour
+                        ?>
+                            <option value="<?php echo $colour; ?>"><?php echo $colour; ?></option>
+                        <?php
+                    }
+                ?>
+            </select>
+        </div>
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'Količina' : 'Pieces'; ?></label>
+            <input type="number" placeholder="250">
+        </div>
+        <div class="form-divider">
+             <span><?php echo ($language == 'HR') ? 'Detalji naručitelja' : 'Customer details'; ?></span>
+        </div>
+        <div class="input-group input-group-label-140">
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'Ime' : 'Name'; ?></label>
+                <input type="text" placeholder="Hrvojka">
+            </div>
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'Firma' : 'Company'; ?></label>
+                <input type="text" placeholder="Hrvojka d.o.o.">
+            </div>
+        </div>
+        <div class="input-group input-group-label-140">
+            <div class="input-field">
+                <label>E-mail</label>
+                <input type="text" placeholder="hrvojka69@gmail.com">
+            </div>
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'TEL/MOB' : 'Phone'; ?></label>
+                <input type="number" placeholder="0912345678">
+            </div>
+        </div>
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'OIB' : 'PID'; ?></label>
+            <input type="number" placeholder="12345678910">
+        </div>
+        <div class="form-divider">
+            <span><?php echo ($language == 'HR') ? 'Detalji dostave' : 'Delivery details'; ?></span>
+        </div>  
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'Adresa dostave 1' : 'Address 1'; ?></label>
+            <input type="text" placeholder="Adresa">
+        </div>
+        <div class="input-field">
+            <label><?php echo ($language == 'HR') ? 'Adresa dostave 2' : 'Address 2'; ?></label>
+            <input type="text" placeholder="Adresa">
+        </div>
+        <div class="input-group input-group-label-160">
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'Grad' : 'City'; ?></label>
+                <input type="text" placeholder="Solin">
+            </div>
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'Županija' : 'Region'; ?></label>
+                <input type="number" placeholder="Splitsko dalmatinska">
+            </div>
+        </div>
+        <div class="input-group input-group-label-160">
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'P. broj' : 'Postal code'; ?></label>
+                <input type="number" placeholder="21210">
+            </div>
+            <div class="input-field">
+                <label><?php echo ($language == 'HR') ? 'Država' : 'Country'; ?></label>
+                <input type="number" placeholder="Srbija">
+            </div>
+        </div>
+        <div class="input-field input-field-extended">
+            <label><?php echo ($language == 'HR') ? 'Dodatan komentar' : 'Additional comment'; ?> </label>
+            <textarea type="text" placeholder="moj komentar" minheight="125"></textarea>
+        </div>
+    </main>
+
 
     <aside class="order-details">
         <h1><?php echo ($language == 'HR') ? 'Dodatne &' : 'Additional &'; ?><br/><?php echo ($language == 'HR') ? 'dostavljačke opcije' : 'shipping options'; ?></h1>
@@ -18,10 +119,11 @@
         <div class="order-details-break">
         <img src="<?php echo images; ?>/title-break.png" alt="">
         </div>
-        <?php MaterialCheckbox('unchecked', '-full', 'underprint', 'info'); ?>
-        <?php MaterialCheckbox('empty', '-full-dotted', 'primer', 'info'); ?>
-        <?php MaterialCheckbox('empty', '-lines', 'uv', 'info'); ?>
-        <?php MaterialCheckbox('empty', '', 'engrave', 'info'); ?>
+        <?php 
+          foreach ($options as $opt) {
+            MaterialCheckbox($opt->checkbox, $opt->icon, $opt->name, $opt->info);
+          }
+        ?>
         <a class="contact-upload" href="#">
             <img src="<?php echo images; ?>/icons/upload.svg" alt="">
             <span><?php echo ($language == 'HR') ? 'Učitaj sliku' : 'Upload artwork'; ?> <br/> <?php echo ($language == 'HR') ? '(opcijonalno)' : '(optional)'; ?></span>
