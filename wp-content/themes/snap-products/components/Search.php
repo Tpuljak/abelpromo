d12a2ae0e7f5ae95590089a0bdfc4e62f4c00c2a
Dražen Barić <drazen.baric@skica.org>
2019-07-18 17:09:54 +0200

    a

--- wp-content/themes/snap-products/components/Search.php
+++ wp-content/themes/snap-products/components/Search.php
@@ -1,8 +1,8 @@
 <?php function Search() { ?>
     <div class="search">
         <input type="text" placeholder="Search">
-        <div>
-            <!-- <img /> -->
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <img style="max-width: 60%" src="<?php echo images; ?>/icons/search-icon.svg" />
         </div>
     </div>
 <?php } ?>
